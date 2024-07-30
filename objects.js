89317ec8679fcfe43b45c0f784a0a50327c0ce9a
Keshav Sharma <keshav15151@gmail.com>
2024-07-30 20:55:42 +0530

    learning objects

---
+++ objects.js
@@ -0,0 +1,10 @@
+let x = {
+    name:"Keshav",
+    age:20,
+    gender:'male',
+    'is married': false
+}
+
+console.log(x['is married']);
+console.log(x.age);
+console.log(x['age']); //always string to be used in the brackets.
