9299f75b1f39f6fab7792518fdcf916f98713343
Keshav Sharma <keshav15151@gmail.com>
2024-07-31 01:59:00 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,12 @@
+function oneToN(n){
+    for (let i = 1; i<=n; i++){
+        console.log(i);
+    }
+}
+oneToN(6);
+
+function fun(a,b){
+    return a*a + b*b
+}
+
+console.log(fun(1,2))
--- objects.js
+++ objects.js
@@ -4,7 +4,19 @@ let x = {
     gender:'male',
     'is married': false
 }
-
+console.log(x);
 console.log(x['is married']);
 console.log(x.age);
 console.log(x['age']); //always string to be used in the brackets.
+
+//object values can be changed
+x.age = 21;
+console.log(x.age);
+
+//for each loop to print all the keys, notice we used x[key] and not x.key
+for (const key in x) {
+    if (Object.hasOwnProperty.call(x, key)) {
+        console.log(key,x[key]);
+    }
+}
+
